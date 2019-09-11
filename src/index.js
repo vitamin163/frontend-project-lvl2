99f28a2fbf1281981a788f71f6e2148c1c624f39
Vitaliy D <vitalik.domnin@gmail.com>
2019-09-11 14:48:06 +0400

    add -format and arguments

--- src/index.js
+++ src/index.js
@@ -5,9 +5,14 @@ export default () => {
   program
     .version('0.0.1')
     .description('Compares two configuration files and shows a difference.')
-    .parse(process.argv);
+    .option('-f, --format [type]', 'Output format')
+    .arguments('<firstConfig> <secondConfig>')
+    .action(() => {
+      console.log(program.arguments);
+      if (!program.args.length) {
+        program.help();
+      }
+    });
 
-  if (!program.args.length) {
-    program.help();
-  }
+  program.parse(process.argv);
 };
