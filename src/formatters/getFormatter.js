73c379fe71138ad0742469a76e35d04c3e25e54b
Vitaliy D <vitalik.domnin@gmail.com>
2019-10-04 12:54:48 +0400

    add json format

--- src/formatters/getFormatter.js
+++ src/formatters/getFormatter.js
@@ -1,10 +1,12 @@
 import defaultFormatter from './defaultFormatter';
 import plainFormatter from './plainFormatter';
+import jsonFormatter from './jsonFormatter';
 
 export default (content, format) => {
   const formatType = {
     default: (ast) => defaultFormatter(ast),
     plain: (ast) => plainFormatter(ast),
+    json: (ast) => jsonFormatter(ast),
   };
   return formatType[format](content);
 };
